94f4f3f4629b4dbdd4b69ca74179f5bd3c2ce9c2
chia313339 <chia313339@gmail.com>
2021-10-13 07:38:28 +0000

    20211013

--- app/models/index.py
+++ app/models/index.py
@@ -3,8 +3,23 @@ import requests
 import json
 import psycopg2
 
+
+# Trello環境參數
+id = '5bb2e4cea274688f9e6f1449'
+key = '7ead9757869a78d5403bcf08e58503a6'
+token = 'ddeda631dc2048c40d340d491b14fd76f046f99ae7177ca075986861113e716a'
+card_id = '61667e8230a32f0dbd4fb789'
+
+# Trello讀取資料
+def get_file_from_trello_card(key, token, card_id):
+    ATTACHMENTS_URL = 'https://api.trello.com/1/cards/%s/attachments'
+    params = {'key': key, 'token': token, 'verify': False}
+    url = ATTACHMENTS_URL % card_id
+    return requests.get(url, params=params)
+
+
 def get_wiki_recentchange():
-    url = "https://gitlab.com/chia313339/treestudio/-/raw/main/data.txt"
+    url = json.loads(get_file_from_trello_card(key, token, card_id).text)[0]['url']
     result = requests.get(url)
     result.encoding = 'utf8'
     return json.loads(result.json())
